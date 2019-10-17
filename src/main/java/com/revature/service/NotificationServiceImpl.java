0f6141327aee06ce3febb83e77f8c25fdfee481f
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-10-16 20:36:56 -0400

    Added all pojos, servlets, services and daos.

---
+++ src/main/java/com/revature/service/NotificationServiceImpl.java
@@ -0,0 +1,5 @@
+package com.revature.service;
+
+public class NotificationServiceImpl implements NotificationService {
+
+}
--- src/main/java/com/revature/servlet/ReimbursementServlet.java
+++ src/main/java/com/revature/servlet/ReimbursementServlet.java
@@ -7,16 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class TestServlet
+ * Servlet implementation class ReimbursementServlet
  */
-public class TestServlet extends HttpServlet {
+public class ReimbursementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReimbursementServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Did I finally make it in?");
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
