b1f18190092a165f7e38a8500e2dfd0544985672
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-10-17 21:58:33 -0400

    Implemented UserServiceImpl with tests

--- src/main/java/com/revature/service/UserServiceImpl.java
+++ src/main/java/com/revature/service/UserServiceImpl.java
@@ -1,5 +1,43 @@
 package com.revature.service;
 
-public class UserServiceImpl implements UserService {
+import com.revature.dao.UserDAO;
+import com.revature.dao.UserDAOImpl;
+import com.revature.pojo.User;
 
+public class UserServiceImpl implements UserService {
+	private UserDAO userDao = new UserDAOImpl();
+
+	@Override
+	public User loginUser(String username, String password) {
+		if (username == null || password == null) {
+			throw new NullPointerException();
+		}
+		
+		User user = null;
+		
+		user = userDao.readUser(username);
+		if (user != null) {
+			if (!user.getPassword().equals(password)) {
+				user = null;
+			}
+		}
+		
+		return user;
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean deleteUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setDao(UserDAO userDao) {
+		this.userDao = userDao;
+	}
 }
