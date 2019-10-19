73c8c0ae55dec09ff046aacce66f94af83aa000e
Carmichael <carmichael901230@gmail.com>
2019-10-19 17:35:40 -0400

    modified user_table

--- sql_query/insert_values.sql
+++ sql_query/insert_values.sql
@@ -1,1 +1,2 @@
-INSERT INTO "Users"(id, username, password, firstname, lastname, role) values(1, 'testuser', 'password', 'Test', 'User', 'EMPLOYEE');
+INSERT INTO "Users"(id, username, password, firstname, lastname, role, supervisor) 
+values(1, 'testuser', 'password', 'Test', 'User', 'EMPLOYEE');
