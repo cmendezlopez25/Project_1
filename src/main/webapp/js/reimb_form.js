d4add77566373a68ac89e6e9d617350e68fcb449
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-10-24 20:58:11 -0400

    Added dateEvent into the Javascript and changed ReimbursementServlet

--- src/main/webapp/js/reimb_form.js
+++ src/main/webapp/js/reimb_form.js
@@ -23,13 +23,13 @@ let updateListItems = [];
 
 function createReimbursement(){
     let reimbursement = new Reimbursement();
-    reimbursement.id = document.getElementById("reimb-id").innerHTML;
     reimbursement.dateCreated = reimbursement.dateLastModified = document.getElementById("create-date").innerHTML;
     reimbursement.type = document.getElementById("inputState").value.toUpperCase();
     reimbursement.message = document.getElementById("comment").value;
     reimbursement.amount = document.getElementById("amount").value;
     reimbursement.status = "PEND_DS";
-    //reimbursement.passgrade = document.getElementById("passGrade").value;
+    reimbursement.dateEvent = document.getElementById("event-date").value;
+    reimbursement.passGrade = document.getElementById("passGrade").value;
     if (inputValidation(reimbursement)){
         let xhr = new XMLHttpRequest();
 
